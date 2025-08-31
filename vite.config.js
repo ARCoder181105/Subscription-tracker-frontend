363e9a880dc6aa2f305024bd84db553920e90a12
Aditya Rana <adityarana181105@gmail.com>
2025-08-31 18:15:41 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ mode }) => {
-
-  return {
-    plugins: [react(), tailwindcss()],
-    build: {
-      outDir: 'dist',
-      sourcemap: true,
-    },
-  }
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  preview: {
+    // ✅ ensures SPA fallback: unknown routes -> index.html
+    historyApiFallback: true,
+  },
 })
